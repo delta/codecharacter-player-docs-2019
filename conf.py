8f7c0f78c3e1b7b8eac936e2d3518012fdaef04e
venkat24 <venkatramansrikanth@outlook.com>
2019-02-26 03:16:57 +0530

    Revert "Change Sphinx theme"
    This reverts commit b48d72d99cfd6e7a953f19164176dc92a7685daf.
    
    The new theme was ugly when inserted as an iframe on the main site

--- conf.py
+++ conf.py
@@ -20,7 +20,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import stanford_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -83,9 +82,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "stanford_theme"
-html_theme_path = [stanford_theme.get_html_theme_path()]
-
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
